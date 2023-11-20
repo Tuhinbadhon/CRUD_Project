ea4b2aa2bd98161f155251b9c8146ec2ae45b330
Md.Fazlul Haque <tuhinbadhon@gmail.com>
2023-11-20 12:00:32 +0600

    first

---
+++ src/components/Employees.js
@@ -0,0 +1,9 @@
+const Employees = [
+  { id: "1", Name: "badhon", Age: "30" },
+
+  { id: "2", Name: "mistu", Age: "33" },
+
+  { id: "3", Name: "jahid", Age: "26" },
+];
+
+export default Employees;
